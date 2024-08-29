d917f1a9c06da69fdb24023fad3084f1ef0bedfd
okanay <okanay@hotmail.com>
2024-08-29 22:16:37 +0300

    reset btn add

--- src/components/fixed-link-group.tsx
+++ src/components/fixed-link-group.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import Link from "next/link";
 
 export const FixedLinkGroup = () => {
+  const handleOnReset = () => {
+    localStorage.clear();
+    location.reload();
+  };
+
   return (
     <div className={"fixed right-8 top-4 sm:top-8"}>
       <div className={"flex flex-col items-end gap-2"}>
@@ -22,6 +29,14 @@ export const FixedLinkGroup = () => {
             Contact
           </span>
         </Link>
+        <button
+          onClick={handleOnReset}
+          className="w-fit rounded-lg border border-zinc-300 bg-zinc-100 px-4 py-2 shadow shadow-zinc-950/10"
+        >
+          <span className="bg-gradient-to-br from-red-400 to-rose-400 bg-clip-text font-mono text-sm font-bold tracking-wider text-transparent">
+            Reset
+          </span>
+        </button>
       </div>
     </div>
   );
